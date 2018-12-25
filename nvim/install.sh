d269b9b95cd02d2bd3d426f3a4b5187143f893bf
sashee <gsashee@gmail.com>
2018-12-25 11:46:50 +0000

    nvim install plugins

--- nvim/install.sh
+++ nvim/install.sh
@@ -5,3 +5,8 @@ chmod u+x /tmp/nvim.appimage
 (cd ~ && /tmp/nvim.appimage --appimage-extract)
 
 ln -s $(pwd -P)/nvim/init.vim ~/.config/nvim/init.vim
+
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+~/squashfs-root/usr/bin/nvim +PlugInstall +UpdateRemotePlugins +qall
