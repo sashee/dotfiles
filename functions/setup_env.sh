191f11c0a58a69958c034cc9953a8dc4f44f1bf6
sashee <gsashee@gmail.com>
2019-02-16 18:14:02 +0100

    fix setup script

--- functions/setup_env.sh
+++ functions/setup_env.sh
@@ -1,13 +1,9 @@
 set -Eeuo pipefail
 
-aws() {
-        ~/.local/bin/aws --region eu-central-1 "$@"
-}
-
 eval `aws sts get-session-token --serial-number $(aws iam list-mfa-devices --user-name $(aws sts get-caller-identity | jq -r '.Arn | split("/")[1]')| jq -r '.MFADevices[0].SerialNumber') --token-code $2 | jq -r '"export AWS_ACCESS_KEY_ID=" + .Credentials.AccessKeyId, "export AWS_SECRET_ACCESS_KEY="+.Credentials.SecretAccessKey, "export AWS_SESSION_TOKEN="+.Credentials.SessionToken'`
 
 eval `ssh-agent`
 
-ssh-add <(aws ssm get-parameter --name $1 --with-decryption | jq -r '.Parameter.Value' | sed 's/\\n/\n/g')
+ssh-add <(aws --region eu-central-1 ssm get-parameter --name $1 --with-decryption | jq -r '.Parameter.Value' | sed 's/\\n/\n/g')
 
 /bin/bash
