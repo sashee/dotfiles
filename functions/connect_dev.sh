53d42156547195d7c8329c046c8870155eda7fa2
Tamás Sallai <82075+sashee@users.noreply.github.com>
2019-02-19 17:09:10 +0100

    Update connect_dev.sh

--- functions/connect_dev.sh
+++ functions/connect_dev.sh
@@ -1,5 +1,6 @@
 while true ; do
-        ssh -tA $1 AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN=$AWS_SESSION_TOKEN tmux new-session -A -s main ;
+        XPORT=$((netstat -nutlp 2> /dev/null | grep :600 | awk '{print $4}' | awk -F: '{print $2}' ; echo 6000) | sort | tail -1)
+        ssh -R 6000:localhost:"$XPORT" -CtA $1 AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN=$AWS_SESSION_TOKEN tmux new-session -A -s main
         sleep 1;
 done
 # No true-color support in 1.3.2. Need to try again when a new version is released
