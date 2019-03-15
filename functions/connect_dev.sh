6729fe6b38bfb356002fbc0cfa6dc1ddfda16ec3
sashee <gsashee@gmail.com>
2019-03-15 19:08:55 +0100

    fixes to the connect script

--- functions/connect_dev.sh
+++ functions/connect_dev.sh
@@ -1,37 +1,40 @@
 dir=$(dirname "$BASH_SOURCE[0]");
 
+SG=$(aws --region eu-central-1 ec2 describe-instances --filter "Name=tag:Name,Values=$2" --query "Reservations[].Instances[].SecurityGroups[].GroupId" --no-paginate | jq -r '.[0]')
+
+MYIP=$(curl -s ifconfig.me)
+
 function setup_sg () {
-	SG=$(aws --region eu-central-1 ec2 describe-instances --filter "Name=tag:Name,Values=$1" --query "Reservations[].Instances[].SecurityGroups[].GroupId" --no-paginate | jq -r '.[0]')
-
-	CIDRS=$(aws --region eu-central-1 ec2 describe-security-groups --group-ids $SG | jq -r '.SecurityGroups[].IpPermissions[].IpRanges[].CidrIp')
-
-	MYIP=$(curl -s ifconfig.me)
+	CIDRS=$(aws --region eu-central-1 ec2 describe-security-groups --group-ids $1 | jq -r '.SecurityGroups[].IpPermissions[] | select(.FromPort == 22 and .ToPort == 22) | .IpRanges[].CidrIp')
 
 	for ip in $CIDRS; do
-		[ "$MYIP/32" != $ip ] && aws --region eu-central-1 ec2 revoke-security-group-ingress --group-id $SG --protocol tcp --port 22 --cidr $ip
+		[ "$2/32" != $ip ] && aws --region eu-central-1 ec2 revoke-security-group-ingress --group-id $1 --protocol tcp --port 22 --cidr $ip
 	done
 
-	[ -z $(echo "$CIDRS" | grep "$MYIP/32") ] && aws --region eu-central-1 ec2 authorize-security-group-ingress --group-id $SG --protocol tcp --port 22 --cidr $MYIP/32
+	[ -z $(echo "$CIDRS" | grep "$2/32") ] && aws --region eu-central-1 ec2 authorize-security-group-ingress --group-id $1 --protocol tcp --port 22 --cidr $2/32
 }
+
+setup_sg $SG $MYIP
 
 STATE=$(aws --region eu-central-1 ec2 describe-instances --filter "Name=tag:Name,Values=$2" --query "Reservations[].Instances[].State.Name" --no-paginate | jq -r '.[0]')
 if [ "$STATE" != "running" ]
 then
-	setup_sg $2
 	$dir/start_instance.sh $2 || exit
 fi
 
 while true ; do
-	setup_sg $2
+	CURRENTMYIP=$(curl -s ifconfig.me)
+	[ "$CURRENTMYIP" != "$MYIP" ] && setup_sg $SG $CURRENTMYIP
+	MYIP="$CURRENTMYIP"
 
 	IP=$(aws --region eu-central-1 ec2 describe-instances --filter "Name=tag:Name,Values=$2" --query "Reservations[].Instances[].NetworkInterfaces[].PrivateIpAddresses[].Association.PublicIp" --no-paginate | jq -r '.[0]')
 
 	mkdir -p $3
+	trap 'kill $(jobs -p) 2> /dev/null' EXIT
 	watch "find $3 -maxdepth 1 -mindepth 1 -exec "$dir/transfer_files.sh" $1@$IP {} \; -exec rm -r {} \;" > /dev/null &
-	pid=$!
 
         ssh -o "StrictHostKeyChecking no" -q -L 0.0.0.0:8080:localhost:8080 -L 0.0.0.0:3000:localhost:3000 -L 0.0.0.0:3001:localhost:3001 -L 0.0.0.0:8443:localhost:443 -L 0.0.0.0:35729:localhost:35729 -L 0.0.0.0:9229:localhost:9229 -L 0.0.0.0:5901:localhost:5901 -CtA $1@$IP AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN=$AWS_SESSION_TOKEN tmux new-session -A -s main
-	kill $pid > /dev/null
+	kill $(jobs -p) 2> /dev/null
         sleep 1;
 done
 
