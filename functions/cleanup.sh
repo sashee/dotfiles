588411ad5aa6a1176fb2d164f27d05c6afa3092c
Tamas Sallai <gsashee@gmail.com>
2019-03-16 10:06:48 +0000

    Cleanup

--- functions/cleanup.sh
+++ functions/cleanup.sh
@@ -1,2 +1,6 @@
-find ~ -name "node_modules" -type d -prune -exec rm -rf '{}' +
+find ~/workspace -name "node_modules" -type d -prune -exec rm -rf '{}' +
 yes | docker system prune -a
+npm cache clean --force
+yarn cache clean
+sudo apt-get autoremove -y --purge
+sudo apt-get clean -y
