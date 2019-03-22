c3558198f7e732f691ada8604c7917b3ce21b273
sashee <gsashee@gmail.com>
2019-03-22 15:13:30 +0100

    retry ip

--- functions/connect_dev.sh
+++ functions/connect_dev.sh
@@ -1,8 +1,13 @@
+set -x
+
 dir=$(dirname "$BASH_SOURCE[0]");
 
 SG=$(aws --region eu-central-1 ec2 describe-instances --filter "Name=tag:Name,Values=$2" --query "Reservations[].Instances[].SecurityGroups[].GroupId" --no-paginate | jq -r '.[0]')
 
-MYIP=$(curl -s ifconfig.me)
+while : ; do
+	MYIP=$(curl -s ifconfig.me)
+	[ -z "$MYIP" ] || break
+done
 
 function setup_sg () {
 	CIDRS=$(aws --region eu-central-1 ec2 describe-security-groups --group-ids $1 | jq -r '.SecurityGroups[].IpPermissions[] | select(.FromPort == 22 and .ToPort == 22) | .IpRanges[].CidrIp')
@@ -23,7 +28,11 @@ then
 fi
 
 while true ; do
-	CURRENTMYIP=$(curl -s ifconfig.me)
+	while : ; do
+		CURRENTMYIP=$(curl -s ifconfig.me)
+		[ -z "$CURRENTMYIP" ] || break
+	done
+
 	[ "$CURRENTMYIP" != "$MYIP" ] && setup_sg $SG $CURRENTMYIP
 	MYIP="$CURRENTMYIP"
 
@@ -33,7 +42,10 @@ while true ; do
 	trap 'kill $(jobs -p) 2> /dev/null' EXIT
 	watch "find $3 -maxdepth 1 -mindepth 1 -exec "$dir/transfer_files.sh" $1@$IP {} \; -exec rm -r {} \;" > /dev/null &
 
-        ssh -o "StrictHostKeyChecking no" -q -L 0.0.0.0:8080:localhost:8080 -L 0.0.0.0:3000:localhost:3000 -L 0.0.0.0:3001:localhost:3001 -L 0.0.0.0:8443:localhost:443 -L 0.0.0.0:35729:localhost:35729 -L 0.0.0.0:9229:localhost:9229 -L 0.0.0.0:5901:localhost:5901 -CtA $1@$IP AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN=$AWS_SESSION_TOKEN tmux new-session -A -s main
+	echo "Connecting"
+        ssh -q -o "StrictHostKeyChecking no" -L 0.0.0.0:8080:localhost:8080 -L 0.0.0.0:3000:localhost:3000 -L 0.0.0.0:3001:localhost:3001 -L 0.0.0.0:8443:localhost:443 -L 0.0.0.0:35729:localhost:35729 -L 0.0.0.0:9229:localhost:9229 -L 0.0.0.0:5901:localhost:5901 -L 0.0.0.0:4200:localhost:4200 -CtA $1@$IP AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN=$AWS_SESSION_TOKEN tmux new-session -A -s main
+	clear;
+	echo "Connection closed"
 	kill $(jobs -p) 2> /dev/null
         sleep 1;
 done
