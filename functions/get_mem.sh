fecc88e937a96702b1e3ee93dc07ba32ece7df68
Tamas Sallai <gsashee@gmail.com>
2019-04-06 08:27:21 +0000

    Print used memory

--- functions/get_mem.sh
+++ functions/get_mem.sh
@@ -1,2 +1,7 @@
-echo "$(awk '/MemAvailable/{printf "%.1f", $2 / 1000000}' /proc/meminfo)/$(awk '/MemTotal/{printf "%.1f", $2 / 1000000}' /proc/meminfo)"
+MEM_FREE=$(awk '/MemAvailable/{print $2 / 1000000}' /proc/meminfo)
+MEM_TOTAL=$(awk '/MemTotal/{print $2 / 1000000}' /proc/meminfo)
+
+MEM_USED=$(echo "$MEM_TOTAL - $MEM_FREE" | bc)
+
+printf "%.1f/%.1f" "$MEM_USED" "$MEM_TOTAL"
 
