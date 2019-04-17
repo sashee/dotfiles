703c9cc6e4e69e37f54497a4a93f552349b8818a
Tamas Sallai <gsashee@gmail.com>
2019-04-17 16:17:40 +0200

    Install curl before oh-my-zsh

--- oh-my-zsh/install.sh
+++ oh-my-zsh/install.sh
@@ -1,4 +1,4 @@
-sudo apt install -y zsh
+sudo apt install -y zsh curl
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sed 's:env zsh -l::g' | sed 's:chsh -s .*$::g')"
 
