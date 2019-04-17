cbdeb36aed596a75f5ce679b0bf97357dbd05966
Tamas Sallai <gsashee@gmail.com>
2019-04-17 16:39:46 +0200

    revert oh-my-zsh overwriting the zsh config

--- oh-my-zsh/install.sh
+++ oh-my-zsh/install.sh
@@ -2,3 +2,4 @@ sudo apt install -y zsh curl
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sed 's:env zsh -l::g' | sed 's:chsh -s .*$::g')"
 
+mv ~/.zshrc.pre-oh-my-zsh ~/.zshrc
