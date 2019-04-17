9c6cb00199c0d621be851bb93b051211bc7f7c0b
Tamas Sallai <gsashee@gmail.com>
2019-04-17 16:08:46 +0200

    fix oh-my-zsh script

--- oh-my-zsh/install.sh
+++ oh-my-zsh/install.sh
@@ -1,3 +1,4 @@
 sudo apt install -y zsh
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sed 's:env zsh -l::g' | sed 's:chsh -s .*$::g')"
+
