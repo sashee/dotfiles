c885ac2c6c348a1d495d48849e681c5d89fa85ea
Tamas Sallai <gsashee@gmail.com>
2019-04-18 11:05:49 +0200

    Install yarn

--- nvim/install.sh
+++ nvim/install.sh
@@ -4,17 +4,34 @@ sudo apt install -y python3-pip
 
 pip3 install --user neovim
 
+# node
 curl -sSL https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo apt-key add -
+
+if [ -e "/etc/apt/sources.list.d/nodesource.list" ]; then
+	sudo rm /etc/apt/sources.list.d/nodesource.list
+fi
+
 VERSION=node_11.x
 DISTRO="$(lsb_release -s -c)"
 echo "deb https://deb.nodesource.com/$VERSION $DISTRO main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 echo "deb-src https://deb.nodesource.com/$VERSION $DISTRO main" | sudo tee -a /etc/apt/sources.list.d/nodesource.list
 sudo apt-get update
 
-# curl -sL https://deb.nodesource.com/setup_11.x | sudo bash -
 sudo apt-get install -y nodejs
 
 npm config set prefix ~/npm
+
+# yarn
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+
+if [ -e "/etc/apt/sources.list.d/yarn.list" ]; then
+	sudo rm /etc/apt/sources.list.d/yarn.list
+fi
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update
+
+sudo apt-get install -y yarn
+
 
 npm install -g eslint
 
@@ -30,8 +47,6 @@ mkdir -p ~/.config/nvim && ln -s $(pwd -P)/nvim/init.vim ~/.config/nvim/init.vim
 
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-~/squashfs-root/usr/bin/nvim +'PlugInstall --sync' +UpdateRemotePlugins +'qall!'
 
 ~/squashfs-root/usr/bin/nvim +'PlugInstall --sync' +UpdateRemotePlugins +'qall!'
 
