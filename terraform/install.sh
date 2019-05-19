39c3e5f00b19cb5f9634aabcfb3d5a7fd9311496
Tamas Sallai <gsashee@gmail.com>
2019-05-19 09:22:50 +0200

    add terraform

---
+++ terraform/install.sh
@@ -0,0 +1,5 @@
+mkdir -p ~/.terraform
+
+zcat <(curl -q $(curl -s https://checkpoint-api.hashicorp.com/v1/check/terraform | jq -r '"\(.current_download_url)terraform_\(.current_version)_linux_amd64.zip"')) > ~/.terraform/terraform
+
+chmod +x ~/.terraform/terraform
