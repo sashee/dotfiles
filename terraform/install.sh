aeb61e9dc18c55e798fa90a3077251ddfb166853
Tamas Sallai <gsashee@gmail.com>
2019-05-19 09:24:39 +0200

    fix terraform install

--- terraform/install.sh
+++ terraform/install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 mkdir -p ~/.terraform
 
 zcat <(curl -q $(curl -s https://checkpoint-api.hashicorp.com/v1/check/terraform | jq -r '"\(.current_download_url)terraform_\(.current_version)_linux_amd64.zip"')) > ~/.terraform/terraform
