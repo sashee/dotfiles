14f3d9ec0b01c1bde52db509b09cabc450182230
Tamas Sallai <gsashee@gmail.com>
2019-10-24 11:23:17 +0200

    cleanup

--- functions/cleanup.sh
+++ functions/cleanup.sh
@@ -1,6 +1,7 @@
 find ~/workspace -name "node_modules" -type d -prune -exec rm -rf '{}' +
 yes | docker system prune -a
 npm cache clean --force
+rm -rf ~/.npm/_npx
 yarn cache clean
 sudo apt-get autoremove -y --purge
 sudo apt-get clean -y
