326695e6b928df4717e35fcc7f4d1587458ff179
Tamas Sallai <gsashee@gmail.com>
2020-04-03 18:56:56 +0200

    update cleanup

--- functions/cleanup.sh
+++ functions/cleanup.sh
@@ -1,8 +1,12 @@
 find ~/workspace -name "node_modules" -type d -prune -exec rm -rf '{}' +
 find ~/workspace -name ".terraform" -type d -prune -exec rm -rf '{}' +
 yes | docker system prune -a
+yes | docker volume prune
 npm cache clean --force
 rm -rf ~/.npm/_npx
 yarn cache clean
+find ~/.cache/ -type f -atime +365 -delete
+sudo journalctl --vacuum-time=10d
 sudo apt-get autoremove -y --purge
 sudo apt-get clean -y
+
