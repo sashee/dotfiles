7ff4a6a675787d3c5302a53f38cafc7a42f40863
Tamas Sallai <gsashee@gmail.com>
2020-05-28 12:19:58 +0200

    Cleanup

--- nvim/install.sh
+++ nvim/install.sh
@@ -11,7 +11,7 @@ if [ -e "/etc/apt/sources.list.d/nodesource.list" ]; then
 	sudo rm /etc/apt/sources.list.d/nodesource.list
 fi
 
-VERSION=node_10.x
+VERSION=node_12.x
 DISTRO="$(lsb_release -s -c)"
 echo "deb https://deb.nodesource.com/$VERSION $DISTRO main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 echo "deb-src https://deb.nodesource.com/$VERSION $DISTRO main" | sudo tee -a /etc/apt/sources.list.d/nodesource.list
