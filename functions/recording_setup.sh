4e19aa0c1db6b457a7aa3c84935ad76e08c68c19
Tamas Sallai <gsashee@gmail.com>
2020-06-11 12:02:26 +0200

    Set background for recording

--- functions/recording_setup.sh
+++ functions/recording_setup.sh
@@ -4,8 +4,10 @@
 RC=$(echo 'export PS1="$ "; export RECORDING_MODE=true; watch echo "Set to ~100x26. Current dimensions: \$(tput cols)x\$(tput lines)"')
 
 tmux set -g status off
+echo -ne "\\ePtmux;\\e\\033]11;#fdf6e3\\007\\e\\\\"
 
 bash --rcfile <(echo $RC)
+echo -ne "\\ePtmux;\\e\\033]11;#000000\\007\\e\\\\"
 
 tmux set -g status on
 
