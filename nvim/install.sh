8010aa6aa6073ddac4950c0b425272f6dd65ecd7
Tamas Sallai <gsashee@gmail.com>
2020-12-10 19:01:24 +0100

    Simplify nvim install script

--- nvim/install.sh
+++ nvim/install.sh
@@ -9,32 +9,17 @@ sudo apt-get install -y nodejs npm
 npm config set prefix ~/npm
 
 # yarn
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-
-if [ -e "/etc/apt/sources.list.d/yarn.list" ]; then
-	sudo rm /etc/apt/sources.list.d/yarn.list
-fi
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt-get update
-
-sudo apt-get install -y yarn
-
+npm install -g yarn
 
 npm install -g eslint
 
-sudo apt install -y ack-grep
-
-wget $(curl -s https://api.github.com/repos/neovim/neovim/releases/latest | jq -r '.assets[] | select(.name == "nvim.appimage") | .browser_download_url') -O /tmp/nvim.appimage
-
-chmod u+x /tmp/nvim.appimage
-
-(cd ~ && /tmp/nvim.appimage --appimage-extract)
+sudo apt install -y ack-grep neovim
 
 mkdir -p ~/.config/nvim && ln -s $(pwd -P)/nvim/init.vim ~/.config/nvim/init.vim
 
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-~/squashfs-root/usr/bin/nvim +'PlugInstall --sync' +UpdateRemotePlugins +'qall!'
+nvim +'PlugInstall --sync' +UpdateRemotePlugins +'qall!'
 
-~/squashfs-root/usr/bin/nvim -c ":CocInstall coc-html coc-css coc-json coc-tsserver coc-yaml coc-eslint coc-tslint"
+nvim -c ":CocInstall coc-html coc-css coc-json coc-tsserver coc-yaml coc-eslint coc-tslint"
