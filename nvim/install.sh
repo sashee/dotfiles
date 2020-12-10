1e2a2e7ddc02b746fae3ce9f2e483b20ff680e7e
Tamas Sallai <gsashee@gmail.com>
2020-12-10 18:06:00 +0100

    Use ubuntu-packaged npm

--- nvim/install.sh
+++ nvim/install.sh
@@ -4,20 +4,7 @@ sudo apt install -y python3-pip
 
 pip3 install --user neovim
 
-# node
-curl -sSL https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo apt-key add -
-
-if [ -e "/etc/apt/sources.list.d/nodesource.list" ]; then
-	sudo rm /etc/apt/sources.list.d/nodesource.list
-fi
-
-VERSION=node_12.x
-DISTRO="$(lsb_release -s -c)"
-echo "deb https://deb.nodesource.com/$VERSION $DISTRO main" | sudo tee /etc/apt/sources.list.d/nodesource.list
-echo "deb-src https://deb.nodesource.com/$VERSION $DISTRO main" | sudo tee -a /etc/apt/sources.list.d/nodesource.list
-sudo apt-get update
-
-sudo apt-get install -y nodejs
+sudo apt-get install -y nodejs npm
 
 npm config set prefix ~/npm
 
