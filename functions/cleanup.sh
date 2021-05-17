3fdc403f1cb8ac2e7c46f96cf4b4c5f32973194c
Tamas Sallai <gsashee@gmail.com>
2021-05-17 17:42:17 +0200

    Sudo remove node_modules, cleanup snaps

--- functions/cleanup.sh
+++ functions/cleanup.sh
@@ -1,10 +1,16 @@
-find ~/workspace -name "node_modules" -type d -prune -exec rm -rf '{}' +
+find ~/workspace -name "node_modules" -type d -prune -exec sudo rm -rf '{}' +
 find ~/workspace -name ".terraform" -type d -prune -exec rm -rf '{}' +
 yes | docker system prune -a
 yes | docker volume prune
 npm cache clean --force
 rm -rf ~/.npm/_npx
 yarn cache clean
+
+LANG=en_US.UTF-8 snap list --all | awk '/disabled/{print $1, $3}' |
+    while read snapname revision; do
+        sudo snap remove "$snapname" --revision="$revision"
+    done
+
 find ~/.cache/ -type f -atime +365 -delete
 sudo journalctl --vacuum-time=10d
 sudo apt-get autoremove -y --purge
