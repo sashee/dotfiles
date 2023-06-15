882bc9c983dde0c2779e7a0973702b0b58010b1a
sashee <tamas.sallai@advancedweb.hu>
2023-06-15 15:04:03 +0200

    Remove redshift

--- graphical/install.sh
+++ graphical/install.sh
@@ -15,7 +15,6 @@ declare -p ENABLED > graphical/.saved_variables
 if [[ "$ENABLED" == "y" ]];
 then
 	sudo pacman -S --needed --noconfirm pulseaudio pavucontrol
-	sudo pacman -S --needed --noconfirm redshift
 	sudo pacman -S --needed --noconfirm xorg alacritty xmonad xmobar xmonad-contrib dmenu xscreensaver dunst numlockx
 	sudo pacman -S --needed --noconfirm vlc ffmpeg chromium leafpad flameshot noto-fonts-emoji
 
