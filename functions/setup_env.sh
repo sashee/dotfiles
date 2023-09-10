6db4e35038dc96813d7cd48a5efea99c2657d38a
sashee <tamas.sallai@advancedweb.hu>
2023-09-10 10:08:21 +0200

    setup end

--- functions/setup_env.sh
+++ functions/setup_env.sh
@@ -1,11 +1,23 @@
 set -Eeuo pipefail
 
-eval `aws sts get-session-token --serial-number $(aws iam list-mfa-devices --user-name $(aws sts get-caller-identity | jq -r '.Arn | split("/")[1]')| jq -r '.MFADevices[0].SerialNumber') --token-code $2 | jq -r '"export AWS_ACCESS_KEY_ID=" + .Credentials.AccessKeyId, "export AWS_SECRET_ACCESS_KEY="+.Credentials.SecretAccessKey, "export AWS_SESSION_TOKEN="+.Credentials.SessionToken'`
+TOKEN_CODE=$2
+PASSPHRASE=$3
+
+AWSRESULTS=$(aws sts get-session-token --serial-number $(aws iam list-mfa-devices --user-name $(aws sts get-caller-identity | jq -r '.Arn | split("/")[1]')| jq -r '.MFADevices[0].SerialNumber') --token-code $TOKEN_CODE)
+
+AWSKEYS=$(echo $AWSRESULTS | jq -r '"AWS_ACCESS_KEY_ID=" + .Credentials.AccessKeyId, "AWS_SECRET_ACCESS_KEY="+.Credentials.SecretAccessKey, "AWS_SESSION_TOKEN="+.Credentials.SessionToken' | gzip | gpg --cipher-algo AES256 --symmetric --batch --passphrase $PASSPHRASE --batch | base64 | tr -d '\n')
 
 eval `ssh-agent`
 
-ssh-add <(aws --region eu-central-1 ssm get-parameter --name $1 --with-decryption | jq -r '.Parameter.Value' | sed 's/\\n/\n/g')
+ssh-add <( \
+	AWS_ACCESS_KEY_ID=$(echo $AWSRESULTS | jq -r '.Credentials.AccessKeyId') \
+	AWS_SECRET_ACCESS_KEY=$(echo $AWSRESULTS | jq -r '.Credentials.SecretAccessKey') \
+	AWS_SESSION_TOKEN=$(echo $AWSRESULTS | jq -r '.Credentials.SessionToken') \
+	aws --region eu-central-1 ssm get-parameter --name $1 --with-decryption | jq -r '.Parameter.Value' | sed 's/\\n/\n/g' \
+)
+
+unset AWSRESULTS
 
 RC=$(([ -r ~/.bashrc ] && cat ~/.bashrc); echo 'PS1="\[\e[0;32m\]$ \[\e[m\]"')
 
-bash --rcfile <(echo $RC)
+AWSKEYS=$AWSKEYS bash --rcfile <(echo $RC)
