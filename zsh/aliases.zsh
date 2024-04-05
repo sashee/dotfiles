0d854e37122c167709123f251b1e920e86fb2593
sashee <tamas.sallai@advancedweb.hu>
2024-04-05 19:18:32 +0200

    fixes

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,9 +1,10 @@
 alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
-alias npm='firejail npm'
-alias nvim='firejail nvim'
-alias nix-shell='/usr/sbin/firejail --profile=/home/sashee/dotfiles/firejail/firejail.profile nix-shell'
-alias make='/usr/sbin/firejail --profile=/home/sashee/dotfiles/firejail/firejail.profile make'
+alias npm='firejail --rmenv=AWSKEYS npm'
+alias npx='firejail --rmenv=AWSKEYS npx'
+alias nvim='firejail --rmenv=AWSKEYS --noblacklist=/tmp/tmux-1000 nvim'
+alias nix-shell='/usr/sbin/firejail --rmenv=AWSKEYS --profile=/home/sashee/dotfiles/firejail/firejail.profile nix-shell'
+alias make='/usr/sbin/firejail --rmenv=AWSKEYS --profile=/home/sashee/dotfiles/firejail/firejail.profile make'
 #alias git='/usr/sbin/firejail git'
-alias lazygit='/usr/sbin/firejail --profile=git lazygit'
+alias lazygit='/usr/sbin/firejail --rmenv=AWSKEYS --profile=git lazygit'
