92131ff11971d8c683be465fd9d22d9f3031ee63
sashee <tamas.sallai@advancedweb.hu>
2025-06-10 17:53:31 +0200

    fix eslint default

---
+++ eslint/eslint.config.js
@@ -0,0 +1,23 @@
+export default [
+	{
+	    "rules": {
+		"indent": [
+		    "error",
+		    "tab"
+		],
+		"linebreak-style": [
+		    "error",
+		    "unix"
+		],
+		"quotes": [
+		    "error",
+		    "double"
+		],
+		"semi": [
+		    "error",
+		    "always"
+		],
+					"no-console": "off"
+	    }
+	},
+];
---
+++ eslint/install.sh
@@ -0,0 +1,2 @@
+rm -f ~/eslint.config.js
+ln -s $PWD/eslint/eslint.config.js ~/eslint.config.js
