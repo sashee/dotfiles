0e454ac21348bc69f7cd3035b8e2920fa22f77f0
sashee <tamas.sallai@advancedweb.hu>
2025-06-19 10:32:57 +0200

    cleanup

--- auto_shutdown/install.sh
+++
@@ -1,9 +0,0 @@
-if $(curl -s -m 5 http://169.254.169.254/latest/dynamic/instance-identity/document | grep -q availabilityZone) ; then
-	cat auto_shutdown/shutdown_if_no_sessions.timer | envsubst | sudo tee /etc/systemd/system/shutdown_if_no_sessions.timer > /dev/null
-
-	cat auto_shutdown/shutdown_if_no_sessions.service | USERNAME="$(id -u -n)" GROUPNAME="$(id -g -n)" envsubst | \
-		sudo tee /etc/systemd/system/shutdown_if_no_sessions.service > /dev/null
-
-	sudo systemctl enable shutdown_if_no_sessions.timer
-	sudo systemctl start shutdown_if_no_sessions.timer
-fi
--- auto_shutdown/shutdown_if_no_sessions.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ $(tmux ls | sed '/^\s*$/d' | wc -l) -eq "0" ]; then
-	sleep 60
-	if [ $(tmux ls | sed '/^\s*$/d' | wc -l) -eq "0" ]; then
-		sudo shutdown -h now
-	fi
-fi
-
