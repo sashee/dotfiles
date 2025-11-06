b1c14d6603bf86ae2719195bc7b41fe2c1b611a0
sashee <tamas.sallai@advancedweb.hu>
2025-11-06 20:41:16 +0100

    finish

--- nix-utils/default2.nix
+++ nix-utils/default2.nix
@@ -4,59 +4,38 @@ let
 		# postInstallCheck fails on rpi kernel, disable it here
 		(final: prev: {landrun = prev.landrun.overrideAttrs (old: {postInstallCheck = "";});})
 	]; };
-	awsv2 = import ./aws/v2.nix {
-		inherit pkgs;
-	};
-	bluetuith = import ./bluetuith/v2.nix {
-		inherit pkgs;
-	};
-	chromium = import ./chromium/v2.nix {
-		inherit pkgs;
-	};
-	duckdb = import ./duckdb/v2.nix {
-		inherit pkgs;
-	};
-	flameshot = import ./flameshot/v2.nix {
-		inherit pkgs;
-	};
-	fx = import ./fx/v2.nix {
-		inherit pkgs;
-	};
-	isd = import ./isd/v2.nix {
-		inherit pkgs;
-	};
-	k2pdfopt = import ./k2pdfopt/v2.nix {
-		inherit pkgs;
-	};
-	keepassxc = import ./keepassxc/v2.nix {
-		inherit pkgs;
-	};
-	lazygit = import ./lazygit/v2.nix {
-		inherit pkgs;
-	};
-	lazysql = import ./lazysql/v2.nix {
-		inherit pkgs;
-	};
-	libreoffice = import ./libreoffice/v2.nix {
-		inherit pkgs;
-	};
-	magicwormhole = import ./magic-wormhole/v2.nix {
-		inherit pkgs;
-	};
-	opencode = import ./opencode/v2.nix {
-		inherit pkgs;
-	};
-	vlc = import ./vlc/v2.nix {
-		inherit pkgs;
-	};
-	nvim = import ./nvim/v2.nix {
-		inherit pkgs;
-	};
-	npm = import ./npm/v2.nix {
-		inherit pkgs;
-	};
+
+	# Programs to pass to zsh for dynamic requirements merging
+	zsh_programs = [
+		(import ./aws/v2.nix { inherit pkgs; })
+		(import ./duckdb/v2.nix { inherit pkgs; })
+		(import ./flameshot/v2.nix { inherit pkgs; })
+		(import ./fx/v2.nix { inherit pkgs; })
+		(import ./isd/v2.nix { inherit pkgs; })
+		(import ./k2pdfopt/v2.nix { inherit pkgs; })
+		(import ./lazygit/v2.nix { inherit pkgs; })
+		(import ./lazysql/v2.nix { inherit pkgs; })
+		(import ./magic-wormhole/v2.nix { inherit pkgs; })
+		(import ./opencode/v2.nix { inherit pkgs; })
+		(import ./vlc/v2.nix { inherit pkgs; })
+		(import ./nvim/v2.nix { inherit pkgs; })
+		(import ./npm/v2.nix { inherit pkgs; })
+	];
+
+	# Programs not passed to zsh (have unrestricted filesystem)
+	other_programs = [
+		(import ./chromium/v2.nix { inherit pkgs; })
+		(import ./keepassxc/v2.nix { inherit pkgs; })
+		(import ./libreoffice/v2.nix { inherit pkgs; })
+		(import ./bluetuith/v2.nix { inherit pkgs; })
+	];
+
+	zsh = import ./zsh/v2.nix { inherit pkgs; prgs = zsh_programs; };
+	tmux = import ./tmux/v2.nix { inherit zsh pkgs; };
+
+	programs = zsh_programs ++ other_programs ++ [zsh tmux];
 in
 	pkgs.buildEnv {
 		name = "scripts-env";
-		paths = awsv2.scripts ++ bluetuith.scripts ++ chromium.scripts ++ duckdb.scripts ++ flameshot.scripts ++ fx.scripts ++ isd.scripts ++ k2pdfopt.scripts ++ keepassxc.scripts ++ lazygit.scripts ++ lazysql.scripts ++ libreoffice.scripts ++ magicwormhole.scripts ++ opencode.scripts ++ vlc.scripts ++ nvim.scripts ++ npm.scripts;
+		paths = builtins.concatLists (map (p: p.scripts) programs);
 	}
