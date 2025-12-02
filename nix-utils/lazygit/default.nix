2082cf8672f6b08c410b5a23ade1c55a00b88a15
sashee <tamas.sallai@advancedweb.hu>
2025-12-02 20:26:15 +0100

    migrate to bwrap

--- nix-utils/lazygit/default.nix
+++ nix-utils/lazygit/default.nix
@@ -5,33 +5,21 @@ let
 	bin = "${pkgs.lazygit}/bin/lazygit";
 	landrun_restrictions = {
 		fs = {
-			"/usr" = "rox";
-			"/dev" = "rox";
-			"/nix" = "rox";
-			"/run/systemd/resolve" = "rox";
-			"/dev/null" = "rwx";
-			"/dev/ptmx" = "rwx";
-			"/dev/pts" = "rwx";
-			"/dev/tty" = "rwx";
-			"(if set -q TMPDIR; echo $TMPDIR; else; echo \"/tmp\"; end)" = "rwx";
-			"/etc/ssl" = "ro";
-			"/etc" = "ro";
 			"~/.ssh/known_hosts" = "ro";
 			"~/.gitconfig" = "ro";
 			"~/.config/lazygit" = "rwx";
+			"~/.local/state/lazygit" = "rwx";
+			"$SSH_AUTH_SOCK" = "rwx";
 		};
 		env = ["HOME" "PATH" "TMPDIR" "TERM" "LANG" "SSH_AUTH_SOCK"];
-		network = {
-			tcp = {
-				connect = [22 443];
-			};
-		};
+		network = {};
 	};
-	before = ''
+ 	before = ''
 export PATH="${
 	pkgs.lib.makeBinPath [
 		pkgs.git
 		pkgs.openssh
+		pkgs.tree
 	]
 }"
 	'';
@@ -41,7 +29,7 @@ export PATH="${
 	'';
 in
 {
-	scripts = (import ../wrapper.nix {
+	scripts = (import ../wrapper2.nix {
 		name = "lazygit";
 		inherit pkgs bin landrun_restrictions before landrun_setup;
 	}).scripts;
