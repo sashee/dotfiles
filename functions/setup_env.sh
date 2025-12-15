fd81763bd94c929e2f7245b3b64a107aab15b99a
sashee <tamas.sallai@advancedweb.hu>
2025-12-15 10:15:46 +0100

    remove ssh

--- functions/setup_env.sh
+++ functions/setup_env.sh
@@ -7,15 +7,6 @@ AWSRESULTS=$(aws sts get-session-token --serial-number $(aws iam list-mfa-device
 
 AWSKEYS=$(echo $AWSRESULTS | jq -r '"AWS_ACCESS_KEY_ID=" + .Credentials.AccessKeyId, "AWS_SECRET_ACCESS_KEY="+.Credentials.SecretAccessKey, "AWS_SESSION_TOKEN="+.Credentials.SessionToken' | gzip | gpg --cipher-algo AES256 --symmetric --batch --passphrase $PASSPHRASE --batch | base64 | tr -d '\n')
 
-eval `ssh-agent`
-
-ssh-add -c <( \
-	AWS_ACCESS_KEY_ID=$(echo $AWSRESULTS | jq -r '.Credentials.AccessKeyId') \
-	AWS_SECRET_ACCESS_KEY=$(echo $AWSRESULTS | jq -r '.Credentials.SecretAccessKey') \
-	AWS_SESSION_TOKEN=$(echo $AWSRESULTS | jq -r '.Credentials.SessionToken') \
-	aws --region eu-central-1 ssm get-parameter --name $1 --with-decryption | jq -r '.Parameter.Value' | sed 's/\\n/\n/g' \
-)
-
 unset AWSRESULTS
 
 RC=$(([ -r ~/.bashrc ] && cat ~/.bashrc); echo 'PS1="\[\e[0;32m\]$ \[\e[m\]"')
